350f73e83c33800f101e31089d073f335750100a
Julian Naranjo <julian.naranjo2014@gmail.com>
2021-05-13 17:44:40 -0500

    task 0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,25 +3,15 @@
 
 
 def minOperations(n):
-    """return an integer"""
+    """return int"""
     if n < 2:
         return 0
-    factor_list = []
+    mul_list = []
     i = 1
     while n != 1:
         i += 1
         if n % i == 0:
             while n % i == 0:
                 n /= i
-                factor_list.append(i)
-    return sum(factor_list)
-        
-
-
-def multiples(n):
-    multiples = []
-    for i in range(1, n +1):
-        module = n%i
-        if module == 0 and i != n:
-            multiples.append(i)
-    return(sorted(multiples)[-2:])
+                mul_list.append(i)
+    return sum(mul_list)
